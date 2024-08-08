8d69d1843f8020aa42e699bcb0d43bdae35b658a
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-08-08 16:45:45 +0000

    m

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-Main
+import tkinter as tk
+
+root = tk.Tk() 
+root.title("My Desktop Application")
+label = tk.Label(root, text="Hello, World!") 
+label.pack()  
+button = tk.Button(root, text="Click Me", command=lambda: print("Button clicked!")) 
+button.pack()
